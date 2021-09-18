28b2337237ed428d8efe6e381258632e5d7b8708
Arian Nasr <anasr@my.yorku.ca>
2021-09-17 21:29:39 -0400

    Initial Commit

---
+++ Waterminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Waterminder
+//
+//  Created by Arian Nasr on 2021-09-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
